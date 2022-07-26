6155c0c348d730afc605aeca751d614268edffc3
sjy <stseo012@gmail.com>
2022-07-26 16:14:03 +0900

    feat: 보유한 동전들을 보여주는 기능

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,5 +1,38 @@
 /* eslint-disable no-prototype-builtins */
 import { tableStyle } from '../style';
+export const printCoinPossession = (coins: ICoin) => {
+    return `
+        <table style='${tableStyle}'>
+        <tr>
+            <td style='${tableStyle}'>동전</td>
+            <td style='${tableStyle}'>개수</td>
+        </tr>
+        <tr>
+            <td style='${tableStyle}'>500원</td>
+            <td id="vending-machine-coin-500-quantity" style='${tableStyle}'>${
+        coins['500'] ? coins['500'] + '개' : ''
+    }</td>
+        </tr>
+        <tr>
+            <td style='${tableStyle}'>100원</td>
+            <td id="vending-machine-coin-100-quantity" style='${tableStyle}'>${
+        coins['100'] ? coins['100'] + '개' : ''
+    }</td>
+        </tr>
+        <tr>
+            <td style='${tableStyle}'>50원</td>
+            <td id="vending-machine-coin-50-quantity" style='${tableStyle}'>${
+        coins['50'] ? coins['50'] + '개' : ''
+    }</td>
+        </tr>
+        <tr>
+            <td style='${tableStyle}'>10원</td>
+            <td id="vending-machine-coin-10-quantity
+            " style='${tableStyle}'>${coins['10'] ? coins['10'] + '개' : ''}</td>
+        </tr>
+        </table>
+        `;
+};
 
 export const getInputValue = (target: HTMLElement, id: string) => {
     return (target.querySelector(`#${id}`) as HTMLInputElement).value;
