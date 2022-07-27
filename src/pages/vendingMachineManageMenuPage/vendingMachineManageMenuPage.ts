3b816b5b0e3c8354b8ad597a3cd58593c8af2213
Jin1won <wlsdnjs16521@gmail.com>
2022-07-28 02:19:56 +0900

    [feat]: 화면에 동전 갯수가 출력되게 한다.

--- src/pages/vendingMachineManageMenuPage/vendingMachineManageMenuPage.ts
+++ src/pages/vendingMachineManageMenuPage/vendingMachineManageMenuPage.ts
@@ -4,6 +4,7 @@ import {
   getLocalStorageItem,
   setLocalStorageItem,
 } from '../../utils/localStorage';
+import { isCoinValid } from '../../utils/validate';
 import './vendingMachineManageMenuPage.css';
 
 class VendingMachineManageMenuPage extends CommonComponent {
@@ -21,32 +22,92 @@ class VendingMachineManageMenuPage extends CommonComponent {
           </tr>
           <tr>
               <td>500원</td>
-              <td>갯수</td>
+              <td id="coin-500"></td>
           </tr>
           <tr>
               <td>100원</td>
-              <td>갯수</td>
+              <td id="coin-100"></td>
           </tr>
           <tr>
               <td>50원</td>
-              <td>갯수</td>
+              <td id="coin-50"></td>
           </tr>
           <tr>
               <td>10원</td>
-              <td>갯수</td>
+              <td id="coin-10"></td>
           </tr>
       </table>
     `;
   }
 
-  renderTotalAmount(): void {
+  saveTotalAmount(): void {
     const totalAmout = domSelector('#vending-machine-charge-input').value;
-    domSelector('#total-amount').innerHTML = totalAmout;
-    setLocalStorageItem('coins', { totalAmount: totalAmout });
+
+    let coinList = getLocalStorageItem('coins');
+    if (coinList === null) {
+      setLocalStorageItem('coins', {
+        500: 0,
+        100: 0,
+        50: 0,
+        10: 0,
+        totalAmout: totalAmout,
+      });
+      return;
+    }
+    coinList['totalAmount'] = totalAmout;
+    setLocalStorageItem('coins', coinList);
+  }
+
+  renderTotalAmount(): void {
+    domSelector('#total-amount').innerHTML =
+      getLocalStorageItem('coins')['totalAmount'];
+  }
+
+  calculateCoins(chargeInput: number) {
+    if (!isCoinValid(chargeInput)) {
+      return;
+    }
+
+    let calculatedInputCoins = {};
+    const coinUnits = [500, 100, 50, 10];
+    let charge = chargeInput;
+
+    coinUnits.forEach((unit) => {
+      const coinQuantity = Math.floor(charge / unit);
+      calculatedInputCoins[unit] = coinQuantity;
+      charge -= coinQuantity * unit;
+    });
+    return calculatedInputCoins;
+  }
+
+  renderCoins(): void {
+    domSelector('#coin-500').innerHTML = getLocalStorageItem('coins')[500];
+    domSelector('#coin-100').innerHTML = getLocalStorageItem('coins')[100];
+    domSelector('#coin-50').innerHTML = getLocalStorageItem('coins')[50];
+    domSelector('#coin-10').innerHTML = getLocalStorageItem('coins')[10];
+  }
+
+  renderCallback(): void {
+    this.renderTotalAmount();
+    this.renderCoins();
   }
 
   onVendingMachineChargeButtonClick(): void {
-    this.renderTotalAmount();
+    this.saveTotalAmount();
+
+    const caculatedCoins = this.calculateCoins(
+      Number(domSelector('#vending-machine-charge-input').value),
+    );
+    let coinList = getLocalStorageItem('coins');
+
+    coinList[500] = caculatedCoins[500];
+    coinList[100] = caculatedCoins[100];
+    coinList[50] = caculatedCoins[50];
+    coinList[10] = caculatedCoins[10];
+
+    setLocalStorageItem('coins', coinList);
+
+    this.renderCallback();
   }
 
   setEvent(): void {
